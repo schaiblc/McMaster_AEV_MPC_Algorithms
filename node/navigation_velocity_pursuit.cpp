a26bddc18dc987dc835e9563927f2598841306c6
schaiblc <90476207+schaiblc@users.noreply.github.com>
2025-06-13 21:08:56 -0400

    Update navigation_velocity_pursuit.cpp

--- node/navigation_velocity_pursuit.cpp
+++ node/navigation_velocity_pursuit.cpp
@@ -396,11 +396,11 @@ void delta_inequality_con(unsigned m, double *result, unsigned n, const double* 
 		}
 
 	}
-	result[2*nMPC*kMPC-1]=x[nMPC*kMPC]-opt_params[3]; //opt_params[3] is the last delta (from previous iteration)
-	result[2*nMPC*kMPC]=-x[nMPC*kMPC]+opt_params[3]; //Can't change servo instantly so delta[0] is fixed
+	result[2*nMPC*kMPC-2]=x[nMPC*kMPC]-opt_params[3]; //opt_params[3] is the last delta (from previous iteration)
+	result[2*nMPC*kMPC-1]=-x[nMPC*kMPC]+opt_params[3]; //Can't change servo instantly so delta[0] is fixed
 	if(grad){
-		grad[(2*nMPC*kMPC-1)*n+nMPC*kMPC]=1;
-		grad[(2*nMPC*kMPC)*n+nMPC*kMPC]=-1;
+		grad[(2*nMPC*kMPC-2)*n+nMPC*kMPC]=1;
+		grad[(2*nMPC*kMPC-1)*n+nMPC*kMPC]=-1;
 	}
 	if(grad){
 		for(int i=0; i<n*m;i++){
@@ -3397,7 +3397,7 @@ class GapBarrier
 				nlopt_add_equality_mconstraint(opt, nMPC*kMPC-1, theta_equality_con, &opt_params, tol);
 				nlopt_add_equality_mconstraint(opt, nMPC*kMPC-1, x_equality_con, &opt_params, tol);
 				nlopt_add_equality_mconstraint(opt, nMPC*kMPC-1, y_equality_con, &opt_params, tol);
-				nlopt_add_inequality_mconstraint(opt, 2*nMPC*kMPC+1, delta_inequality_con, &opt_params, tol1);
+				nlopt_add_inequality_mconstraint(opt, 2*nMPC*kMPC, delta_inequality_con, &opt_params, tol1);
 				nlopt_add_inequality_mconstraint(opt, 4*nMPC*kMPC-2, vel_inequality_con, opt_params_vel.data(), tol2);
 				if(leader_detect==1){
 					nlopt_add_inequality_mconstraint(opt, 1, pursuit_inequality_con, opt_params_pursuit.data(), tolp);
